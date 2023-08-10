d0b938af2c6b406c5458750e18a7b187c5f44203
hanzi17 <116144434+hanzi17@users.noreply.github.com>
2023-08-10 16:43:50 +0900

    로그인/인덱스페이지로 이동

--- app.py
+++ app.py
@@ -9,16 +9,14 @@ ca = certifi.where()
 client = MongoClient('mongodb+srv://sparta:JEix6NjqkUe2HlnI@cluster0.dx5eieg.mongodb.net/?retryWrites=true&w=majority',tlsCAFile=ca)
 db = client.dbsparta
 
-
 @app.route('/')
 def home():
     userInt_receive = request.cookies.get('userInt')
-    return render_template('index.html')
-
-@app.route('/login')
-def loginPage():
     return render_template('login.html')
 
+@app.route('/index')
+def mainPage():
+    return render_template('index.html')
 
 # 몽고DB에 닉네임, 이메일, 비밀번호 데이터 넣기
 @app.route("/login", methods=["POST"])
@@ -29,34 +27,33 @@ def login():
     nick_receive = request.form['nick_give']
     
     doc = {
-        'userInt': userInt,
+        'nickname' : nick_receive,
         'id': id_receive,
         'pw': pw_receive,
-        'nickname' : nick_receive,
-    }
-    userInt += 1 
+    } 
 
-    # id, 암호화된 pw을 가지고 해당 유저를 찾습니다.
+    # id, 암호화된pw을 가지고 해당 유저를 찾습니다.
     result = db.user.find_one({'id': id_receive, 'pw': pw_receive, 'nickname': nick_receive})
 
     # 찾으면 JWT 토큰을 만들어 발급합니다.
     if result is not None:
 
         # token을 줍니다.
-        return jsonify({'result': 'success', 'nickname': result['nickname']})
+        return jsonify({'result': 'success', 'userInt': result['userInt']})
     # 찾지 못하면
     else:
         return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
 
 
-
 # 몽고DB에 to-do-list 데이터 넣기
 @app.route("/todo", methods=["POST"])
 def todo_post():
+    userInt_receive = int(request.cookies.get('userInt'))
     date_receive = request.form['date_give']
     list_receive = request.form['list_give']
 
     doc = {
+        'userInt' : userInt_receive,
         'date': date_receive,
         'list': list_receive,
         'done' : 0
@@ -88,18 +85,24 @@ def todo_delete():
     return jsonify({'msg': '삭제 완료'})
 
 
-#몽고디비에서 num 값 대신 DB 고유값인 ID값 가져오기
-#ID를 가져오는 이유는 완료 표시를 하기 위해서!
+#userInt 가져오기
+@app.route("/userInt", methods=["GET"])
+def userInt():
+    userInt_receive = int(request.cookies.get('userInt'))
+    userinfo = db.user.find_one({'userInt': userInt_receive}, {'_id': 0})
+    
+    return jsonify({'result': userinfo, 'userInt':userInt_receive})
+
+#로그인한 유저 정보
 @app.route("/todo", methods=["GET"])
-def todo_get():
-    userInt_receive = request.cookies.get('userInt')
-    userinfo = db.user.find_one({'userInt': userInt_receive})
+def todo_get():    
+    userInt_receive = int(request.cookies.get('userInt'))
+    todoinfo = list(db.todo.find({'userInt': userInt_receive}))
 
-    todo_memo_list = list(db.todo.find({}))
-
-    for i in range(len(todo_memo_list)):
-        todo_memo_list[i]['_id'] = str(todo_memo_list[i]['_id'])
-    return jsonify({'todo_memos': todo_memo_list, 'userIntNum':userinfo})
+    for i in range(len(todoinfo)):
+        todoinfo[i]['_id'] = str(todoinfo[i]['_id'])
+    
+    return jsonify({'result': todoinfo, 'userInt': userInt_receive})
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5001, debug=True)    
+    app.run('0.0.0.0', port=5001, debug=True)
