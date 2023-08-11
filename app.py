ea7fd2604772b4033986404e852042e100d5f5b3
hanzi17 <116144434+hanzi17@users.noreply.github.com>
2023-08-11 09:25:23 +0900

    userInt 값과 닉네임 찾아서 넘기기

--- app.py
+++ app.py
@@ -2,42 +2,58 @@ from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 from bson.objectid import ObjectId
 
-# 파이몽고 연결하기
-from pymongo import MongoClient
-client = MongoClient('mongodb+srv://pmaker126:test@cluster0.pemxrix.mongodb.net/?retryWrites=true&w=majority')
+from pymongo import MongoClient 
+import certifi
+
+ca = certifi.where()
+client = MongoClient('mongodb+srv://sparta:JEix6NjqkUe2HlnI@cluster0.dx5eieg.mongodb.net/?retryWrites=true&w=majority',tlsCAFile=ca)
 db = client.dbsparta
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    userInt_receive = request.cookies.get('userInt')
+    return render_template('login.html')
 
+@app.route('/index')
+def mainPage():
+    return render_template('index.html')
 
 # 몽고DB에 닉네임, 이메일, 비밀번호 데이터 넣기
 @app.route("/login", methods=["POST"])
 def login():
+    userInt = 0
     id_receive = request.form['id_give']
     pw_receive = request.form['pw_give']
     nick_receive = request.form['nick_give']
+    
+    doc = {
+        'nickname' : nick_receive,
+        'id': id_receive,
+        'pw': pw_receive,
+    } 
 
     # id, 암호화된pw을 가지고 해당 유저를 찾습니다.
-    result = db.todo.find_one({'email': id_receive, 'pw': pw_receive, 'nick': nick_receive})
+    result = db.user.find_one({'id': id_receive, 'pw': pw_receive, 'nickname': nick_receive})
 
     # 찾으면 JWT 토큰을 만들어 발급합니다.
     if result is not None:
 
         # token을 줍니다.
-        return jsonify({'result': 'success'})
+        return jsonify({'result': 'success', 'userInt': result['userInt']})
     # 찾지 못하면
     else:
         return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
 
-# 몽고DB에 to-do-list 데이터 넣기
+
+#몽고DB에 to-do-list 데이터 넣기
 @app.route("/todo", methods=["POST"])
 def todo_post():
+    userInt_receive = int(request.cookies.get('userInt'))
     date_receive = request.form['date_give']
     list_receive = request.form['list_give']
 
     doc = {
+        'userInt' : userInt_receive,
         'date': date_receive,
         'list': list_receive,
         'done' : 0
@@ -52,30 +68,42 @@ def todo_done():
     id_receive = request.form['id_give']
     db.todo.update_one({'_id': ObjectId(id_receive)}, {'$set': {'done': 1}})
 
-    return jsonify({'msg': '완료!'})
+    return jsonify({'msg': '완료 개굴!'})
 
 #투두 완료 취소 
 @app.route("/todo/cancel", methods=["POST"])
 def todo_cancel():
     id_receive = request.form['id_give']
     db.todo.update_one({'_id': ObjectId(id_receive)}, {'$set': {'done': 0}})
-    return jsonify({'msg': '완료 취소!'})
+    return jsonify({'msg': '완료 취소 개굴!'})
 
 #투두 삭제
 @app.route("/todo/delete", methods=["POST"])
 def todo_delete():
     id_receive = request.form['id_give']
     db.todo.delete_one({'_id': ObjectId(id_receive)})
-    return jsonify({'msg': '삭제 완료'})
+    return jsonify({'msg': '삭제 완료 개굴!'})
 
-#몽고디비에서 num 값 대신 ID값 가져오기
+
+#userInt 가져오기
+@app.route("/userInt", methods=["GET"])
+def userInt():
+    userInt_receive = int(request.cookies.get('userInt'))
+    userinfo = db.user.find_one({'userInt': userInt_receive}, {'_id': 0})
+    
+    return jsonify({'result': userinfo, 'userInt':userInt_receive})
+
+#로그인한 유저 정보
 @app.route("/todo", methods=["GET"])
-def todo_get():
-    todo_memo_list = list(db.todo.find())
+def todo_get():    
+    userInt_receive = int(request.cookies.get('userInt'))
+    nickname_receive = db.user.find_one({'userInt': userInt_receive})['nickname']
+    todoinfo = list(db.todo.find({'userInt': userInt_receive}))
 
-    for i in range(len(todo_memo_list)):
-        todo_memo_list[i]['_id'] = str(todo_memo_list[i]['_id'])
-    return jsonify({'todo_memos': todo_memo_list})
+    for i in range(len(todoinfo)):
+        todoinfo[i]['_id'] = str(todoinfo[i]['_id'])
+    
+    return jsonify({'result': todoinfo, 'userInt': userInt_receive, 'nickname': nickname_receive })
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5001, debug=True)
